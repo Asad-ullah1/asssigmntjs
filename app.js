3c91cf387b1342343930902bf287b5678a529371
Asad <52581107+Asad-ullah1@users.noreply.github.com>
2022-04-10 12:49:24 +0500

    commit2
    updated

---
+++ app.js
@@ -0,0 +1,531 @@
+//chapter 1
+// Questio 1
+// alert("Hello User");
+
+// Question 2
+// alert("Error! Please enter a valid Password");
+
+// Question 3
+// alert("Welcome to JS land...\n Happy Coding!");
+
+// Questio 4
+// alert("Welcome to JS land...")
+// alert("Happy Coding!\n Prevent this page from creating additional dialogs.")
+
+// Question 5
+// alert("Hello... I Can JS through my web browser's Console")
+      //  alert("Error! please inter a valid password")
+
+      //  alert("Welcome to js Land..\n Happy Coding ")
+      //  alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+
+      //  alert("Hello..I Can run JS through my web browser's Console")
+// queston 6 
+  // document.write("<h1>Hello World</h1>");
+
+
+  //chapter 2
+
+  //     Question 1
+
+// var userName;
+
+
+//------ Question 2 ------
+
+// var myname = "Asad";    .. declared and assigned
+
+///----- Question 3 
+
+// var message = "Hello World!";
+// alert(message);                ..// message in alert box
+
+
+
+ // Quesation 4       
+     /*  var username;
+        var myName = "Asadullah";
+        age = " i am 22 years old"
+        var message = "Hellow world";
+        alert(myName)
+        alert(age)
+        alert(message) */
+
+
+//question 5
+/*var display= "pizza";
+alert(display)
+      */
+
+
+//Question 6
+/*var email = "ak659277";
+alert("My email address is: " + email+"@gmail.com");*/
+
+//Question 7
+/*var book = " iam trying to learn smarter way smarter \n way to learn JavaScript";
+alert(book)*/
+
+//Question 8
+/*document.write(" yeh! i can write html code on browser");*/
+
+//Question 9
+
+var line= "“▬▬▬▬▬▬▬▬▬";
+line2 = "▬▬▬▬▬▬▬▬▬”"
+var flower = "ஜ۩۞۩ஜ"
+alert(line+ flower + line2)
+
+//chapter3
+
+      //question 1
+   //   var age = 21;
+    //  alert("i am "+ age+"years old")
+
+//question 2 
+   /*   var birthYear= 1998;
+      document.write("My birthyear is " + birthYear +" <br>")
+      document.write(" data type of variable is number") */
+
+
+//------* Question 3 *------//
+
+// var birthyear = 1990;
+// document.write("My birth year is " +birthyear);
+// document.write("</br> The data type of my birth year is " +typeof(birthyear));
+
+      
+     //question4
+ /*    var name = "jhon dae";
+     var product ="T-SHIRT";
+     var quantity = 5;
+     document.write(name +"\n ordered \n"+ quantity +"\n"+ product +"\n on XYZ cloths")
+*/
+
+//chapter 4
+
+ //question 2
+     // var name = "Asad", userName = "ak6592asad", age = 22;
+    //  document.write("name : "+name +"<br> user name :"+userName+" <br> age is "+age)
+
+
+     //question 2
+    //legal and illegal variable names
+
+    //legal 
+  /*  var age =21;
+    var birthDate=1995;
+    var _class;
+    var num1;
+    var $karachi;
+    //illegal
+    var  %my = "John"
+    var 2my = "John"
+    var -my = "John"
+   var +sign;
+   var T oken; */
+// 
+// ..
+/*
+document.write("Variable names can only contain . number, $ and_. for example :$my_1stvariable ")
+document.write("<br> Variable must begain wth letter.$or_.for example : $name._name or name")
+document.write("<br>variable names arecase sensitive")
+document.write("<br> Varialble names should no be js keywords")
+*/
+//chapter 5
+
+ //question 1 and 2  
+/*
+      var num1 =3;
+      var num2 = 5;
+sum =num1 + num2;
+document.write( "sum of numbers is : "+ sum);
+sub =num1 - num2;
+document.write( "<br> subtration of numbers is : "+ sub);
+multi =num1 * num2;
+document.write( " <br> subtmultiplicationration of numbers is : "+ multi);
+modulos =num1 % num2;
+document.write( " <br> modulos of numbers is : "+ modulos);
+//question 4
+var ticket = 600;
+var quantity = 5;
+total_cost= ticket * quantity;
+document.write("<br> Total cost to buy 5 tickets to a movies is " +total_cost +"PKR<br>")
+*/
+//question 5
+/*
+var  t=4;
+var  i=0;
+while(i<=10)
+{
+document.write(t+  "  X  "  +i+  "  =  "  +i*t+   "<br>");
+i++;
+}
+*/
+//question 6
+/*function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(message);
+}
+
+function fToC(fahrenheit) 
+{
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    console.log(message);
+} 
+cToF(60); 
+fToC(45);
+*/
+
+//question 7
+/*
+var item1_price = 650;
+var item2_price = 100;
+quantity1 =3;
+quantity2 =7;
+charges =100;
+document.write("<br><h1>Shopping cart</h1>")
+document.write("<br>price if item  1 is : " + item1_price)
+document.write("<br>Quantity of item 1 is : " + quantity1)
+document.write("<br>price if item  2 is : " + item2_price)
+document.write("<br>Quantity of item 2 is : " + quantity2)
+document.write("<br>shipping charges : " + charges)
+cost_itm1 =item1_price *3;
+cost_itm2 =item2_price *7;
+total = cost_itm1 + cost_itm2 + charges;
+document.write("<br>Total Cost ofyour order is " + total)
+*/
+
+//question 8
+var total_marks =980;
+var marks_obtained =804;
+percentage = marks_obtained / total_marks * 100;
+document.write("<br>Percentage:" + percentage +"%")
+
+//question 9
+/*
+var us_dollar= 1;
+var pkr = 155;
+var sra1 = 28;
+total_usdollar =10 *pkr;
+totaL_sra = 25 * sra1;
+total_pkr =total_usdollar+totaL_sra;
+document.write("<br>Total Currancy in pkr :" +total_pkr)
+*/
+//ques)tion 
+/*var num4=5,num5 =10,num6=2;
+sum = num4 *num5 /num6;
+document.write("<br>"+sum);*/
+
+// question 
+/*
+currentYear =2022;
+birthYear = 2000;
+ageCalc = currentYear - birthYear;
+document.write("<br> Current year is :"+currentYear);
+document.write("<br> Birth year is :"+birthYear);
+document.write("<br> Yourage is : "+ageCalc);
+*/
+
+//question
+/*
+var snak ="chocklate chips";
+var age = 22;
+var maxi_age =68;
+var perDay =3;
+totalSnake = age * perDay;
+document.write("<br>Favorite Snake :" + snak)
+document.write("<br>Current Age :" + age)
+document.write("<br> Estimated maximum age :" + maxi_age)
+document.write("<br>Amaount of snake per day  : :" + perDay)
+document.write("<br> you will need "+ totalSnake +" \n"+ snak +" \nto last you until the ripe old age of   " + maxi_age);
+    
+    */
+
+//chapter 6-9
+
+  //question 1
+  /*
+  document.write("Result : <br>")
+        
+  var a =10;
+  document.write("The value of a is is :" + a + "<br><br>")
+  document.write("..........................................<br>")
+  ++a;
+  document.write("The value of ++a is : "+ a)
+  document.write("<br> Now the value of a is : "+ a)
+  a=11;
+  document.write("<br>The value of a++ is : "+ a)
+
+  a++;
+  document.write("<br> Now the value of a is : "+ a)
+
+  a=12;
+  document.write("<br>  the value of --a is : 11")
+
+  --a;
+  document.write("<br> Now the value of a is : "+ a)
+  a=11;
+  document.write("<br>  the value of a-- is : 11")
+  a--;
+  document.write("<br> Now the value of a is : "+ a)
+*/
+//question 2
+/*
+var a=2;
+var b=1;
+--a;
+document.write("<br>the value of a is : "+ a)
+--a;
+--b;
+c= --a - --b;
+document.write("<br> Now the value of a is : "+ c)
+--a;
+--b;
+++b;
+d= --a - --b  + ++b;
+
+document.write("<br> Now the value of a is : "+ d)
+
+--a;
+--b;
+--b;
+b++;
+total = --a - --b + --b + b--;
+document.write("<br> Now the value of a is : "+ total)
+*/
+// question
+//var name = window.prompt("Enter your name: ");
+//alert("Your name is " + name);
+//question
+/*var number = window.prompt("Enter your number: ");
+var  a=5;
+var  i=0;
+number=i;
+num
+while(i<=10)
+{
+document.write(number+  "  X  "  +number+  "  =  "  +number*a+   "<br>");
+i++;
+}*/
+//question
+/*
+var math;
+var english;
+var urdu;
+t_marks=300;
+var math=prompt("Enter obtain ofmath marks")
+var english=prompt("Enter obtain of english marks")
+var urdu=prompt("Enter obtain of urdu marks")
+sum = math + english + urdu;
+document.write(sum)
+
+per =sum /t_marks *100;
+document.write("<br>"+per)*/
+
+
+//chapter 9-11
+
+ //question 1
+      /*
+      var city;
+      var city=prompt("enter your city plz ")
+      document.write("Welcom to  " + city)
+      */
+     //question 2
+/*
+      document.getElementById("mybtn").value;
+      document.write("pls sir" +mybtn)
+      */
+     //question
+    /*
+     var red;
+     var green;
+     var yellow;
+     var color = prompt("plz type signal color" )
+     if(color == red){document.write("Stop plz")
+     if (color== yellow) {
+         document.write("plz wait 2 min")
+         
+     } else {
+         document.write("plz go")
+     }
+
+     }
+     */
+    //question 
+/*
+    var a = 4;
+if (++a === 5){
+alert("given condition for variable a is true");
+}
+var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");
+} var c = 12;
+if (c++ === 13){
+alert("condition 1 is true");
+}
+if (c === 13){
+alert("condition 2 is true");
+}
+if (++c < 14){
+alert("condition 3 is true");
+}
+if(c === 14){
+alert("condition 4 is true");
+}
+ var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");
+}
+ if (true){
+alert("True");
+}
+if (false){
+alert("False");
+}
+if("car" < "cat"){
+alert("car is smaller than cat");
+}
+*/
+
+//question
+/*
+num; 
+divde= num /3;
+var num=prompt("enter your number ")
+document.write("divisible by 3 " + divde)
+*/
+//question
+/*
+
+var number =prompt("enter number")
+if(number % 2 == 0) {
+    document.write("the number is even")
+}
+else{
+    document.write("the number is odd")
+}
+*/
+
+//question
+/*
+
+var temp =prompt("plz input temperature 10 to 40 degress only")
+if(temp>=10){
+    document.write("OMG! Todays weather is so Cool.")}
+    else if (temp>30) {
+        document.write("The Weather today is Normal.")
+    } else {
+        document.write("It is too hot outside.")
+    }
+*/
+
+//question calulator
+
+
+/*
+const number1 = prompt('Enter first number: ');
+const number2 = prompt('Enter second number: ');
+
+
+
+const operator = prompt("Enter operator  +, -, * or /" )
+
+
+let result;
+
+
+if (operator == '+') {
+    result = +number1 + +number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+document.write(+result)
+*/
+//chapter 12-13
+
+  //question1
+        /*
+        var ch = prompt("enter character")
+        if (ch >= 'A' && ch <= 'Z')
+        document.write( " is an UpperCase character <br>");
+ 
+    else if (ch >= 'a' && ch <= 'z')
+    document.write( " is an lowercase character <br>");
+             
+ 
+    else{
+    document.write( " is an not a  character <br>");
+             
+}
+      /*
+var numb=prompt("enter your number")
+if (numb== -1){ document.write("negative naumber<br>")
+}
+else{document.write("poitive naumber<br>")}
+
+*/
+//Question 4
+// var cr= prompt("Enter Character");
+// if (cr == 'a'  || cr == 'e' || cr == 'i' || cr == 'o'  || cr == 'u'){
+//     alert("TRUE")
+// } else{
+//     alert("FALSE")
+// }
+
+//question 5
+/*
+c_password="asad1234";
+var name=prompt("your name")  
+var password=prompt("your password")   
+  
+if (c_password==password){  
+  alert("Correct"); }
+  
+ 
+  else if(c_password!=password){ 
+  alert("Incorrect password");  }
+  else { 
+  alert("Please enter your password");  }
+*/
+
+
+//question6
+
+// var hour;
+// var hour = prompt("Enter hour");
+// if (hour < 18) {
+//    alert("Good day")
+// }
+// else{
+//   alert("Good Evening")
+// }
+
+//question 7
+// var time = 1900;
+// var time = prompt("Input time");
+// if (time >=0000 && time <1200){
+//     alert("Good Morning")
+// } else if (time >=1200 && time <1700){
+//     alert("Good Afternoon")
+// } else if(time >=1700 && time <2100){
+//     alert("Good Evening");
+// } else if (time >=2100 && time <2359){
+//     alert("Good Night")
+// }
